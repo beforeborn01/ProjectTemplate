f2dba774df07c8a7830cc96dddb132fc3f2e7709
sunjianzhi <sunjianzhi@xdf.cn>
2022-10-12 11:27:53 +0800

    temp

--- template-web/src/test/java/com/youneng/troy/template/web/util/HttpTest.java
+++ template-web/src/test/java/com/youneng/troy/template/web/util/HttpTest.java
@@ -1,6 +1,12 @@
 package com.youneng.troy.template.web.util;
 
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpMethod;
 
@@ -17,6 +23,27 @@ public class HttpTest {
     @Test
     public void test(){
         System.out.println(HttpMethod.GET.toString());
+    }
+
+    @Test
+    public void testGroupBy(){
+        long start=System.currentTimeMillis();
+        List<Person> list=new ArrayList<>();
+        for (int i = 0; i < 50000; i++) {
+            list.add(new Person((long) (Math.random()*10000),"name"+Math.random()*10000,"desc---------"+Math.random()*100000));
+        }
+        Map<Long, List<Person>> map = list.stream()
+            .collect(Collectors.groupingBy(Person::getId));
+        System.out.println(map.size());
+        System.out.println("use time="+(System.currentTimeMillis()-start));
+    }
+
+    @Data
+    @AllArgsConstructor
+    class Person{
+        private Long id;
+        private String name;
+        private String desc;
     }
 
 }
