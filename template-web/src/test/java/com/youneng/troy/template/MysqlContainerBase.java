2afea70f71e24e4ec30c4e9dfe08fd598a15e3a0
sunjianzhi <sunjianzhi@xdf.cn>
2023-08-28 15:24:08 +0800

    优化mysql容器配置

--- template-web/src/test/java/com/youneng/troy/template/MysqlContainerBase.java
+++ template-web/src/test/java/com/youneng/troy/template/MysqlContainerBase.java
@@ -22,10 +22,12 @@ public class MysqlContainerBase implements BeforeAllCallback {
     @Override
     public void beforeAll(ExtensionContext context) {
         if (mysql == null) {
-            mysql = (MySQLContainer) new MySQLContainer(DockerImageName.parse("mysql:5.7")).withDatabaseName("demo").withInitScript("table.sql") // 指定数据库初始化脚本，默认的db为test
+            mysql = (MySQLContainer) new MySQLContainer(DockerImageName.parse("mysql:5.7")).withDatabaseName("template").withInitScript("table.sql") // 指定数据库初始化脚本，默认的db为test
                     .withPassword("admin").withUsername("admin"); // 指定登录信息，不指定也可以。指定后可以通过外部工具直接连接mysql进行查看
             mysql.start();
-            System.setProperty("jdbc.url", mysql.getJdbcUrl());// 使用容器对象获取jdbcUrl，会自动设置合适的ip和端口号
+//            System.setProperty("jdbc.url", mysql.getJdbcUrl());// 使用容器对象获取jdbcUrl，会自动设置合适的ip和端口号
+            System.setProperty("tidb.host", "localhost");
+            System.setProperty("tidb.port", mysql.getFirstMappedPort() + "");
             System.setProperty("seal.route.data-source-config-map.masterDatasource.username", mysql.getUsername());
             System.setProperty("seal.route.data-source-config-map.masterDatasource.password", mysql.getPassword());
         }
